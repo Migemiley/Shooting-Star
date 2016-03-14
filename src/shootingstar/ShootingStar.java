20163e8ec5dbf1cb66426da3ef9c7b5023eee2a2
Agnieszka Makowska <migusiaczek15@wp.pl>
2016-03-14 09:10:41 +0100

    Dodanie grafiki wroga

--- src/shootingstar/ShootingStar.java
+++ src/shootingstar/ShootingStar.java
@@ -4,20 +4,29 @@ package shootingstar;
  * @author Agnieszka Makowska https://github.com/Migemiley
  */
 
+// Dodanie odpowiednich bibliotek
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.*;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import java.util.HashMap;
 
 public class ShootingStar extends Canvas {
+	// Dodanie globalnych zmiennych dotyczących szerokości i wysokości okna aplikacji
 	public static final int szer = 800;
 	public static final int wys = 600;
+	public HashMap sprites;
 	
+	// Konstruktor ShootingStar(), w którym zostaje zainicjowane okno aplikacji
 	public ShootingStar(){
+		sprites = new HashMap();
+		
 		JFrame okno = new JFrame("Shooting Star");
 		JPanel panel = (JPanel)okno.getContentPane();
 		setBounds(0,0,szer,wys);
@@ -28,19 +37,43 @@ public class ShootingStar extends Canvas {
 		okno.setVisible(true);
 		
 		okno.addWindowListener(new WindowAdapter(){
+			@Override
 			public void windowClosing(WindowEvent e){
 				System.exit(0);
 			}
 		});
 	}
 	
+	// Metoda odpowiadająca za wczytywanie grafik
+	public BufferedImage loadImage(String wczyt){
+		URL url = null;
+		try {
+			url = getClass().getClassLoader().getResource(wczyt);
+			return ImageIO.read(url);
+		} catch (Exception e){
+			System.out.println(wczyt + " " + url);
+			System.out.println("Blad - "+e.getClass().getName()+" "+e.getMessage());
+			System.exit(0);
+			return null;
+		}
+	}
+	
+	public BufferedImage getSprite(String wczyt){
+		BufferedImage img = (BufferedImage)sprites.get(wczyt);
+		if (img == null){
+			img = loadImage("img/"+wczyt);
+			sprites.put(wczyt, img);
+		}
+		return img;
+	}
+	
+	// Metoda odpowiadająca za rysowanie
+	@Override
 	public void paint(Graphics grafika){
-		grafika.setColor (Color.blue);
-		grafika.fillRect(szer/2, wys/3, 100, 200);
+		grafika.drawImage(getSprite("star1.png"), 50, 50, this);
 	}
 	
 	public static void main(String[] args) {
 		ShootingStar inv = new ShootingStar();
 	}
-	
 }
